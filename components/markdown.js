3509b34b0d4989b42e7ee0ae3f4d2feed8c13953
David Glymph <Woozl@users.noreply.github.com>
2024-05-17 15:23:24 -0400

    add remark-gfm and render markdown tables with MUI

--- components/markdown.js
+++ components/markdown.js
@@ -1,7 +1,8 @@
-import { Typography } from '@mui/material'
+import { Paper, Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Typography } from '@mui/material'
 import ReactMarkdown from 'react-markdown'
 import { Link } from './link'
 import { Fragment } from 'react'
+import remarkGfm from 'remark-gfm'
 
 // this object defines a map:
 //    DOM elements --> React components.
@@ -10,32 +11,52 @@ import { Fragment } from 'react'
 
 const componentMap = {
   // for links, we'll use our smart link component.
-  a: ({ node, href, ...props }) => (
+  a: ({ href, ...props }) => (
     <Link
       to={ href }
       { ...props }
     />
   ),
-  p: ({ node, children, ...props })=>(
+  p: ({ children, ...props })=>(
     <Typography paragraph {...props} sx={{ '&:last-of-type': { mb: 0 }}}>{children}</Typography>
   ),
-  h1: ({ node, children, ...props }) => (
+  h1: ({ children, ...props }) => (
     <Typography variant="h3" {...props}>{children}</Typography>
   ),
-  h2: ({ node, children, ...props }) => (
+  h2: ({ children, ...props }) => (
     <Typography variant="h4" {...props}>{children}</Typography>
   ),
-  h3: ({ node, children, ...props }) => (
+  h3: ({ children, ...props }) => (
     <Typography variant="h5" {...props}>{children}</Typography>
   ),
-  ul: ({ node, children, ...props }) => (
+  ul: ({ children, ...props }) => (
     <ul style={{marginTop: 0, lineHeight: '1.8'}} {...props}>{children}</ul>
-  )
+  ),
+  table: ({ children, ...props }) => (
+    <TableContainer component={Paper} sx={{ mt: 2, mb: 4 }}>
+      <Table {...props}>{children}</Table>
+    </TableContainer>
+  ),
+  thead: ({ children, ...props }) => (
+    <TableHead {...props}>{children}</TableHead>
+  ),
+  tbody: ({ children, ...props }) => (
+    <TableBody {...props}>{children}</TableBody>
+  ),
+  tr: ({ children, ...props }) => (
+    <TableRow {...props}>{children}</TableRow>
+  ),
+  th: ({ children, ...props }) => (
+    <TableCell {...props}>{children}</TableCell>
+  ),
+  td: ({ children, ...props }) => (
+    <TableCell {...props}>{children}</TableCell>
+  ),
 }
 
 export const Markdown = ({ children }) => {
   return (
-    <ReactMarkdown components={ componentMap }>
+    <ReactMarkdown components={ componentMap } remarkPlugins={[remarkGfm]}>
       { children }
     </ReactMarkdown>
   )
