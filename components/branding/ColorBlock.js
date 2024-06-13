1c9ec4740791a4312d0f286ab95b5cc0742f054b
suejinkim20 <kim.suejin.20@gmail.com>
2024-06-13 01:24:28 -0400

    refactor ColorBlock to utilize MUI styles and remove unneeded styles

--- components/branding/ColorBlock.js
+++ components/branding/ColorBlock.js
@@ -3,47 +3,21 @@ import { Card, CardContent, Stack } from '@mui/material/'
 import { InfoBlock } from './InfoBlock'
 import { hexToRgb } from '@/utils/hextoRgb'
 
-const styles = {
-  card: {
-    margin: '16px',
-    display: 'flex',
-    flexDirection: 'row',
-  },
-  colorChip: {
-    flex: 1,
-    minWidth: '100px',
-    minHeight: 'auto',
-  },
-  content: {
-    flex: 3,
-    display: 'flex',
-    justifyContent: 'flex-start',
-    flexDirection: 'row',
-    flex: 5,
-    width: '100%',
-  },
-  info: {
-    flex: 1,
-    flexBasis: '33%',
-  }
-}
-
 export const ColorBlock = ({colorName, colorHex}) => {
   const { r, g, b } = hexToRgb(colorHex)
   const colorRgb = `rgb(${[r, g, b].join(', ')})`
   
   return (
-    <Card elevation={3} sx={styles.card}>
-      <CardContent sx={styles.colorChip} style={{ backgroundColor: colorHex }}/>
-      <CardContent sx={styles.content}>
+    <Card elevation={3} sx={{ display: 'flex', flexDirection: 'row' }}>
+      <CardContent sx={{ flex: 1, backgroundColor: colorHex }}/>
+      <CardContent sx={{ flex: 5, m: 0.75 }}>
         <Stack 
           direction={{ sm: 'column', md: 'row' }} 
           spacing={{ xs: 1, sm: 2, md: 4 }}
-          sx={{width: '100%'}}
         >
-          <InfoBlock copyable title="Name" body={ colorName } style={styles.info}/>
-          <InfoBlock copyable title="Hex" body={ colorHex } style={styles.info}/>
-          <InfoBlock copyable title="RGB" body={ colorRgb } style={styles.info}/>
+          <InfoBlock copyable title="Name" body={ colorName } style={{ flexBasis: '33%' }} />
+          <InfoBlock copyable title="Hex" body={ colorHex } style={{ flexBasis: '33%' }} />
+          <InfoBlock copyable title="RGB" body={ colorRgb } style={{ flexBasis: '33%' }} />
         </Stack>
       </CardContent>
     </Card>
