1907fff75f1b85b8ccdf299efaf57792779beda5
suejinkim20 <kim.suejin.20@gmail.com>
2024-06-13 01:46:20 -0400

    refactor LogoBlock to utilize MUI styles and remove unneeded styles

--- components/branding/LogoBlock.js
+++ components/branding/LogoBlock.js
@@ -2,27 +2,14 @@ import React from 'react'
 import { Card, CardContent } from '@mui/material/'
 import Image from 'next/image'
 
-// to do: 
-// consider using usePathname and our custom 
-// Link component when upgrading to Nextjs v13
-
-const styles = {
-  card: {
-    padding: 0,
-    margin: '16px'
-  },
-  topper: {
-    borderBottom: `1px solid grey`,
-  },
-  content: {
-    backgroundColor: 'white',
-  },
-}
-
 export const LogoBlock = ({logo, bgColor}) => {
   return (
-    <Card sx={ styles.card }>
-      <CardContent sx={{ ...styles.topper, backgroundColor: bgColor }}>
+    <Card elevation={3} >
+      <CardContent sx={{ backgroundColor: bgColor }}>
+        {/* 
+          to do in future: consider using usePathname and our 
+          custom Link component when upgrading to Nextjs v13 
+        */}
         <a
           href={logo.src} 
           target="_blank"
