5d148be837433bf2d6986ec13f4e1ac3365970b4
suejinkim20 <kim.suejin.20@gmail.com>
2024-06-13 01:45:57 -0400

    refactor LogoSection to utilize MUI styles and remove unneeded styles

--- components/branding/LogoSection.js
+++ components/branding/LogoSection.js
@@ -2,25 +2,12 @@ import React from 'react'
 import Grid from '@mui/material/Grid'
 import { LogoBlock } from './LogoBlock'
 
-const styles = {
-    logoGrid: {
-        marginBottom: '32px',
-        display: 'flex',
-        justifyContent: 'flex-start',
-        width: '100%',
-    },
-    logoBlock: {
-        flexGrow: 1,
-        width: '100%',
-    },
-}
-
 export const LogoSection = ({logos}) => {
   return (
-    <Grid container sx={ styles.logoGrid }>
+    <Grid container sx={{ mb: 3 }} spacing={3}>
       {
         logos.map( (logo) => {
-          return <Grid item xs={12} md={ 6 } lg={4} sx={ styles.logoBlock } key={ logo.image }>
+          return <Grid item xs={12} md={ 6 } lg={4} key={ logo.image }>
             <LogoBlock logo={ logo.image } bgColor={ logo.bgColor }/>
           </Grid>
         })
