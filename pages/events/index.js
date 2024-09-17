d3244914d473cc68fc43d4f0b8d3e0ba7ab57beb
Matt Watson <mwatson@renci.org>
2024-09-17 11:45:31 -0400

    add events menu item & empty page
    ..remove "home" from main menu

---
+++ pages/events/index.js
@@ -0,0 +1,12 @@
+import { Page } from "@/components/layout";
+
+export default function Events() {
+  return (
+    <Page
+      title="Events"
+      description="..."
+    >
+      &nbsp;
+    </Page>
+  );
+}
