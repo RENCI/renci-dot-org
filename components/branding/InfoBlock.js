ccc477843b8c1116bfc1907e95669f359429fa55
Matt Watson <mwatson@renci.org>
2024-10-01 13:22:55 -0400

    give copy feedback

--- components/branding/InfoBlock.js
+++ components/branding/InfoBlock.js
@@ -1,6 +1,7 @@
-import React from "react";
-import { Typography } from "@mui/material";
+import React, { useEffect, useMemo, useState } from "react";
+import { IconButton, Typography } from "@mui/material";
 import ContentCopyIcon from "@mui/icons-material/ContentCopy";
+import ContentCopiedIcon from "@mui/icons-material/Check";
 import { copyToClipboard } from "@/utils/copyToClipboard";
 
 const styles = {
@@ -20,39 +21,58 @@ const styles = {
     "&:hover": {
       cursor: "pointer",
     },
-    "&:hover .copyIcon": {
+    '.copyButton': {
+      opacity: 0,
+      transform: 'scale(0.5)',
+    },
+    "&:hover .copyButton": {
       opacity: 0.7,
     },
   },
-  copyIcon: {
-    textTransform: "uppercase",
-    marginLeft: "8px",
-    opacity: 0,
-    fontSize: "98%",
-  },
 };
 
-export const InfoBlock = (props) => {
-  const { copyable } = props;
+const CopyButton = ({ copied = false, clickHandler }) => {
+  return (
+    <IconButton
+      onClick={ clickHandler }
+      className="copyButton"
+      color={ copied ? "success" : "default" }
+    >
+      { copied ? <ContentCopiedIcon /> : <ContentCopyIcon /> }
+    </IconButton>
+  );
+};
+
+export const InfoBlock = ({
+  copyable = false,
+  style = {},
+  title = "",
+  body = "",
+}) => {
+  const [copied, setCopied] = useState(false);
+
+  const handleClick = () => setCopied(true);
+
+  useEffect(() => {
+    if (!copied) {
+      return;
+    }
+    const resetTimer = setTimeout(() => setCopied(false), 3000);
+    return () => clearTimeout(resetTimer);
+  }, [copied]);
 
   return (
-    <div style={props.style}>
+    <div style={style}>
       <Typography variant="body1" sx={styles.title}>
-        {props.title}
+        {title}
       </Typography>
       <Typography
         variant="body2"
-        onClick={() => copyToClipboard(props.body)}
+        onClick={() => copyToClipboard(body)}
         sx={copyable ? styles.copyableBody : styles.body}
       >
-        {props.body}
-        {copyable ? (
-          <ContentCopyIcon
-            fontSize="small"
-            sx={styles.copyIcon}
-            className="copyIcon"
-          />
-        ) : null}
+        {body}
+        {copyable && <CopyButton clickHandler={ handleClick } copied={ copied } />}
       </Typography>
     </div>
   );
