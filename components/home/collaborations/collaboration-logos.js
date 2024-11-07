93437fa4d56c54613105f870ec471018e1d11fea
David Glymph <Woozl@users.noreply.github.com>
2024-11-07 14:56:36 -0500

    collaboration cards with description

--- components/home/collaborations/collaboration-logos.js
+++ components/home/collaborations/collaboration-logos.js
@@ -1,4 +1,4 @@
-import { Box } from "@mui/material";
+import { Box, Typography } from "@mui/material";
 import Image from "next/image";
 import Doe from "./images/doe.png";
 import Rti from "./images/rti.png"
@@ -6,26 +6,62 @@ import Nih from "./images/nih.png";
 import Noaa from "./images/noaa.png";
 import Nsf from "./images/nsf.png";
 import Unc from "./images/unc.png";
+import Link from "next/link";
 
 export function CollaborationLogos() {
   return (
     <Box
       sx={{
+        color: "black",
+        p: 4,
         display: "flex",
         flexDirection: "column",
-        gap: "4rem",
-        width: "100%",
-        height: "100%",
         alignItems: "center",
         justifyContent: "center",
+        height: "100%",
+        gap: 4,
       }}
     >
-      <Image src={Nih} layout="fixed" alt="" />
-      <Image src={Noaa} layout="fixed" alt="" />
-      <Image src={Nsf} layout="fixed" alt="" />
-      <Image src={Doe} layout="fixed" alt="" />
-      <Image src={Rti} layout="fixed" alt="" />
-      <Image src={Unc} layout="fixed" alt="" />
+      <Card
+        imgSrc={Nih}
+        link={"/about"}
+        text={"RENCI has partnered with NIH on initiatives such as the RECOVER Data Gateway, BDC3: The Cloud IC Interoperability Framework, and GeneScreen, among others, to develop innovative solutions for data curation, cloud computing, and genetic testing. Through these collaborations, NIH and RENCI are working together to harness the power of data to drive scientific breakthroughs and improve human health."}
+      /> 
+      <Card
+        imgSrc={Noaa}
+        link={"/about"}
+        text={"NOAA has partnered with RENCI on various projects, including the development of new technology and research for the Joint Hurricane Testbed (JHT) to create a \"Weather-Ready Nation.\" RENCI has also collaborated with NOAA's Office of Weather and Air Quality on a grant to enhance storm surge forecasting capabilities at the National Hurricane Center."}
+      /> 
+      <Card
+        imgSrc={Nsf}
+        link={"/about"}
+        text={"The National Science Foundation (NSF) has partnered with RENCI on various projects, including the development of the SciDAS framework to support distributed, data-driven research, and the CI Compass project to accelerate the data lifecycle for NSF Major Facilities. RENCI has also collaborated with NSF on the DataBridge project to extract and use knowledge from large datasets, and the SAFE Superfacilities project to enhance the security and flexibility of science networking."}
+      /> 
     </Box>
   );
+}
+
+function Card({ imgSrc, text, link }) {
+  return (
+    <Box
+      sx={{
+        background: "#fff",
+        boxShadow: "0px 4px 8px 0px #00000052",
+        borderRadius: "8px",
+        p: 2,
+        display: "flex",
+        alignItems: "center",
+        gap: 2,
+        maxWidth: "600px",
+      }}
+    >
+      <Box sx={{ flex: "0 0 140px", display: "flex", alignItems: "center", justifyContent: "center" }}>
+       <Image src={imgSrc} layout="fixed" alt="" />
+      </Box>
+       <Box>
+        <Typography>{text} <Link href={link}>Learn more</Link></Typography>
+        
+       </Box>
+    </Box>
+  )
 }
